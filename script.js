1d9297d0c09abac24cf13b6a4fc79feca2d3f168
KimSweetRain <117064718+KimSweetRain@users.noreply.github.com>
2022-11-16 17:16:32 +0900

    [script, CSS, HTML] 메뉴버튼 액션 수정

--- script.js
+++ script.js
@@ -29,7 +29,6 @@ document.addEventListener('scroll', () => {
     }
     home.style.opacity = 1 - window.scrollY / homeHeight;
 })
-
 // contact 버튼 누르면 이동
 const contact = document.querySelector('.home__contact');
 contact.addEventListener('click', ()=>{
@@ -45,10 +44,113 @@ navbarMenu.addEventListener('click', (e) => {
     if(link == null){
         return;
     }
+    navbarMenu.classList.remove('open');
     scrollIntoView(link);
 })
 
+
+// 모바일 메뉴 버튼 설정
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+    navbarMenu.classList.toggle('open');
+})
+
+
+
+// 작업실 선택
+const workBtnContainer = document.querySelector('.work_categories');
+const projectContainer = document.querySelector('.work__project');
+const projects = document.querySelectorAll('.project');
+
+workBtnContainer.addEventListener('click', (e) => {
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    if(filter == null){
+        return;
+    }
+    const active = document.querySelector('.category__btn.selected');
+    if(active != null){
+        active.classList.remove('selected');
+    }
+    e.target.classList.add('selected');
+
+    projectContainer.classList.add('anim-out')
+    setTimeout(() => {
+        projects.forEach((project) => {
+            console.log(project.dataset.type);
+            if(filter == '*' || filter == project.dataset.type){
+                project.classList.remove('invisible');
+            } else{
+                project.classList.add('invisible');
+            }
+        });
+        projectContainer.classList.remove('anim-out')
+    },300)
+})
+
+
+// 스크롤에 따른 메뉴 테두리
+
 function scrollIntoView(selector){
     const scrollTo = document.querySelector(selector);
-    scrollTo.scrollIntoView({behavior: 'smooth'});
+    scrollTo.scrollIntoView({ behavior: 'smooth' });
+    selectNavItem(navItems[sectionIds.indexOf(selector)]);
 }
+
+const sectionIds = [
+    '#home',
+    '#about',
+    '#skills',
+    '#work1',
+    '#testimonials',
+    '#contact'
+];
+
+const sections = sectionIds.map((id) => document.querySelector(id));
+// console.log(sections)
+const navItems = sectionIds.map((Id) => document.querySelector(`[data-link='${Id}']`))
+// console.log(navItems)
+
+let selectedNavIndex = 0;
+let selectedNavItem = navItems[0];
+
+function selectNavItem(selected) {
+    selectedNavItem.classList.remove('active');
+    selectedNavItem = selected;
+    selectedNavItem.classList.add('active');
+}
+
+const observerOptions = {
+    root : null,
+    rootMargin : '0px',
+    threshold : 0.3
+}
+const observerCallback = (entries, observer) => {
+    entries.forEach((entry) => {
+        if(!entry.isIntersecting && entry.intersectionRatio > 0) {
+            console.log('y');
+            const index = sectionIds.indexOf(`#${entry.target.id}`);
+            // console.log(index)
+            if(entry.boundingClientRect.y < 0) {
+                selectedNavIndex = index + 1;
+            } else {
+                selectedNavIndex =  index -1;
+            }
+            console.log(selectedNavIndex)
+        }
+    })
+}
+
+
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+sections.forEach((section) => observer.observe(section));
+
+window.addEventListener('wheel',() => {
+    if(window.scrollY === 0) {
+        selectedNavIndex = 0;
+    } else if (
+        window.screenY + window.innerHeight === document.body.clientHeight
+    ) {
+        selectedNavIndex = navItems.length - 1;
+    }
+    selectNavItem(navItems[selectedNavIndex]);
+});
